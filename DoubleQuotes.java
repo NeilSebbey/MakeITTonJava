20243d56656ba3ec4f426e203fc1dadfb4da3fbb
Neil P. Sebbey <Neil.P.Sebbey@students.ittralee.ie>
2020-01-23 09:44:34 +0000

    New Files

---
+++ DoubleQuotes.java
@@ -0,0 +1,10 @@
+//LongComments.java
+/*This program illustrates how long text messages (or comments) can be handled in a program 
+ *without having to resort to scrolling*/
+ 
+ public class DoubleQuotes{
+ 	public static void main(String args[])
+ 	{
+ 		System.out.println("\"To be or not to be, that is the question\"");
+ 	}
+ }
---
+++ HelloWorldTest.java
@@ -0,0 +1,9 @@
+//HelloWorldTest.java
+/*This program displays a simple "Hello World" message to the screen*/
+
+public class HelloWorldTest{
+	public static void main (String args[])
+	{
+		System.out.println("Hello World!"); //the message being displayed
+	}
+}
